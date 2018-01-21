ac95ebe4cbf42640704bd4089f4de192d8286045
MaxKavun <developmax@mail.ru>
2018-01-21 12:21:46 +0300

    Add some logic

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // Task lists version 0.1
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+
+function loadEventListeners() {
+  // Add task events
+
+  form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  } else {
+    // Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // Create link for delete element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+    taskInput.value = '';
+  }
+  
+  e.preventDefault();
+}
